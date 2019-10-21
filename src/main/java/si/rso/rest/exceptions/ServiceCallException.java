9407f0be7f8750c6418c365353295e56bf23c33d
Miha Jamsek <miha_jamsek@windowslive.com>
2019-10-21 14:59:06 +0200

    added service name to exception

--- src/main/java/si/rso/rest/exceptions/ServiceCallException.java
+++ src/main/java/si/rso/rest/exceptions/ServiceCallException.java
@@ -4,12 +4,27 @@ import javax.ws.rs.core.Response;
 
 public class ServiceCallException extends RestException {
     
+    private String serviceName;
+    
     public ServiceCallException(String message) {
         super(message, Response.Status.SERVICE_UNAVAILABLE);
+    }
+    
+    public ServiceCallException(String message, String service) {
+        super(message, Response.Status.SERVICE_UNAVAILABLE);
+        this.serviceName = service;
     }
     
     public ServiceCallException(String message, Throwable cause) {
         super(message, Response.Status.SERVICE_UNAVAILABLE, cause);
     }
     
+    public ServiceCallException(String message, String service, Throwable cause) {
+        super(message, Response.Status.SERVICE_UNAVAILABLE, cause);
+        this.serviceName = service;
+    }
+    
+    public String getServiceName() {
+        return serviceName;
+    }
 }
